5120d45c786cdd272eb1d12ac4f2305b54bbd12b
franky1234 <thebigbrother3@gmail.com>
2016-10-21 19:40:30 -0400

    add method getAll whit param .

--- client/app/main/list/java/java.controller.js
+++ client/app/main/list/java/java.controller.js
@@ -1,13 +1,18 @@
 'use strict';
 
 angular.module('uiApp')
-  .controller('JavaCtrl', function ($resource) {
+  .controller('JavaCtrl', function ($scope, $resource) {
     //http://ui-grid.info/  for references.
     var vm = this;
     console.log('inside java....');
-    //vm.students = $resource('/api/cursos/java').then;//
-    console.log('its::::::'+vm.students);
-    vm.myData = [
+    vm.students = $resource('/api/cursos/java');
+    vm.allStudents = vm.students.get({},function(gizmo){
+      //console.log('inside :'+JSON.stringify(gizmo));
+      //$scope.gridOptions = {data : gizmo};
+      vm.gridOptions = gizmo.students;
+    }); 
+    //console.log('its :'+ vm.gridOptions);
+   /* vm.myData = [
                   { 
                     name : 'Pepe' , 
                     age : 20 
@@ -25,5 +30,5 @@ angular.module('uiApp')
                       age : 25 
                     }
                     ];
-  vm.gridOptions = {data : vm.myData };
+  vm.gridOptions = {data : vm.myData };*/
   });
